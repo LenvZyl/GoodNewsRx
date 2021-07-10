ca706ea995fd50ddcb88f42848a556d33a245c0d
Lenx <lenvzyl@gmail.com>
2021-07-10 10:47:34 +0200

    Setup

---
+++ GoodNews/Commons.swift
@@ -0,0 +1,12 @@
+//
+//  Commons.swift
+//  GoodNews
+//
+//  Created by Len van Zyl on 2021/07/10.
+//
+
+import Foundation
+
+class Commons {
+    static let apiKey = "b2a997e7d3f6409c8967fb48d4d411fe"
+}
